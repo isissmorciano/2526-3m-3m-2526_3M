bbcb366aadaae02b4480136910345ca5559d99ab
Rafael <pedrorafael.nicodemo@studenti.isissgobetti.it>
2026-03-19 13:49:42 +0100

    Es.64 Inizializzato

---
+++ src/m10_package/Quiz/Domande.py
@@ -0,0 +1,13 @@
+def crea_domanda(testo: str, opzioni: list[str], risposta: int) -> dict:
+    return {
+        
+    }
+
+def info_domanda(domanda:dict) -> str:
+    pass
+
+def risposta_valida(domanda: dict, scelta: int) -> bool:
+    pass
+
+def verifica_risposta(domanda: dict, scelta: int) -> bool:
+    pass
---
+++ src/m10_package/Quiz/Risultati.py
@@ -0,0 +1,17 @@
+def crea_risultati() -> dict:
+    pass
+
+def registra_risposta(risultati: dict, corretta: bool) -> None:
+    pass
+
+def percentuale_corrette(risultati: dict) -> float:
+    pass
+
+def mostra_risultati(risultati: dict) -> str:
+    pass
+
+def salva_risultati(risultati: dict, nome_file: str) -> None:
+    pass
+
+def carica_risultati(nome_file: str) -> dict:
+    pass
