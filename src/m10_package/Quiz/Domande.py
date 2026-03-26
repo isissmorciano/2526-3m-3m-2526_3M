4c2351afa97e999686d636ec31f5cf11ac5189d2
Rafael <pedrorafael.nicodemo@studenti.isissgobetti.it>
2026-03-26 12:43:01 +0100

    Prova_64

--- src/m10_package/Quiz/Domande.py
+++ src/m10_package/Quiz/Domande.py
@@ -1,13 +1,20 @@
 def crea_domanda(testo: str, opzioni: list[str], risposta: int) -> dict:
     return {
-        
+        "Testo": testo,
+        "Opzioni": opzioni,
+        "Risposta": risposta
     }
 
 def info_domanda(domanda:dict) -> str:
-    pass
+    linee = [domanda["Testo"]]
+    for i, opzione in enumerate(domanda["Opzioni"], 1):
+        linee.append(f"{i}. {opzione}")
+    return "\n".join(linee)
 
 def risposta_valida(domanda: dict, scelta: int) -> bool:
-    pass
+    return 1 <= scelta <= len(domanda["opzioni"])
 
 def verifica_risposta(domanda: dict, scelta: int) -> bool:
-    pass
+    if not risposta_valida(domanda, scelta):
+        return False
+    return (scelta - 1) == domanda["risposta"]
